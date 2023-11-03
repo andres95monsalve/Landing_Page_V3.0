e873bdad82922dd1ed1ad824b04fd5dc5e6ce60f
Melvin Rivas <andres95monsalve@gmail.com>
2023-11-03 13:52:26 -0400

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -2,44 +2,64 @@ import { THREE } from "./three.js/build/three.module.js";
 import { GLTFLoader } from "./three.js/examples/jsm/loaders/GLTFLoader.js";
 import express from "express";
 
-const canvas = document.querySelector("webgl"); // The WebGL canvas element
+// Configuración del servidor
+const app = express();
+
+// Ruta para renderizar la escena
+app.get("/", (req, res) => {
+  // Renderiza la escena Three.js
+  renderer.render(scene, camera);
+
+  // Envia el HTML con la escena
+  res.send(`<html><head><title>Three.js scene</title></head><body><canvas id="canvas" width="600" height="400"></canvas></body></html>`);
+});
+
+// Escucha el puerto 3000
+const server = http.createServer(app);
+server.listen(3000, () => {
+  console.log("Servidor iniciado en http://localhost:3000");
+});
+
+// Configuración de la escena Three.js
+const canvas = document.querySelector("canvas"); // El elemento canvas WebGL
 const scene = new THREE.Scene();
 
-// Create a group to contain the GLB model
+// Crea un grupo para contener el modelo GLB
 const modelGroup = new THREE.Group();
 
+// Carga el modelo GLB
 const loader = new GLTFLoader();
 loader.loadAsync(
   "../assets/helicopter.glb",
   (gltf) => {
     console.log(gltf);
 
-    // Get the center of the GLTF model
+    // Obtiene el centro del modelo GLB
     const center = gltf.scene.getCenter(new THREE.Vector3());
 
-    // Position the GLTF model at the center of the group
+    // Posiciona el modelo GLB en el centro del grupo
     gltf.scene.position.sub(center);
 
-    // Add the GLTF model to the group
+    // Agrega el modelo GLB al grupo
     modelGroup.add(gltf.scene);
 
-    // Add the group to the scene
+    // Agrega el grupo a la escena
     scene.add(modelGroup);
   },
   (xhr) => {
-    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+    console.log((xhr.loaded / xhr.total) * 100 + "% cargado");
   },
   (error) => {
-    console.log("An error occurred: ", error);
+    console.log("Ocurrió un error: ", error);
   }
 );
 
-// Add a light to the scene
+// Agrega una luz a la escena
 const light = new THREE.DirectionalLight("0xffffff", 1);
 light.position.set(2, 2, 5);
 scene.add(light);
 
-// Set up the camera
+// Configura la cámara
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -49,7 +69,7 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.set(0, 1, 2);
 scene.add(camera);
 
-// Set up the renderer
+// Configura el renderizador
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
@@ -58,16 +78,11 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.shadowMap.enabled = true;
 renderer.gammaOutput = true;
 
-// Start the web server
-const app = express();
-
-app.get('/', (req, res) => {
-  // Render the Three.js scene here
+// Renderiza la escena
+function render() {
+  requestAnimationFrame(render);
   renderer.render(scene, camera);
-  res.send('<h1>Three.js scene</h1>');
-});
+}
 
-const server = http.createServer(app);
-server.listen(3000, () => {
-  console.log('Servidor iniciado en http://localhost:3000');
-});
+// Inicia la renderización
+render();
